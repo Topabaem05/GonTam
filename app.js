6488fb959bb026c2cde992bdab6383ef728b9d14
Topabaem05 <87803226+Topabaem05@users.noreply.github.com>
2022-08-16 12:17:47 +0900

    Test

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert(473737)
+const cal = {
+    Plus: function (a,b){
+        return a + b
+    },
+    Minos: function (a,b){
+        return a - b
+    },
+    Times: function (a,b){
+        return a * b
+    },
+    divide: function (a,b){
+        return a / b
+    },
+    Power: function (a,b){
+        return a ** b
+    },
+};
+
+const result = cal.Plus(4,5)
+alert(result)
