14231aed16a7026bbef8ca1d3bb3ce1532b0f94e
Topabaem05 <87803226+Topabaem05@users.noreply.github.com>
2022-08-18 09:42:48 +0900

    Test

--- app1.js
+++ app1.js
@@ -1,2 +1,9 @@
 const age = parseInt(prompt("너는 몇살?"))
-alert(age)
+const Name = prompt("너의 이름")
+ 
+if (isNaN(age)){
+    alert("Plase Write a Number")
+}
+else {
+    alert("Hello" + String(Name))
+}
