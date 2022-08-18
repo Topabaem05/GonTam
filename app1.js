4e7ba6601549bfc1a2c950c6404f8967fe78b004
Topabaem05 <87803226+Topabaem05@users.noreply.github.com>
2022-08-18 10:19:42 +0900

    Test

--- app1.js
+++ app1.js
@@ -1,9 +1,11 @@
 const age = parseInt(prompt("너는 몇살?"))
-const Name = prompt("너의 이름")
  
-if (isNaN(age)){
+if (isNaN(age) || age < 0){
     alert("Plase Write a Number")
-}
-else {
-    alert("Hello" + String(Name))
+}else if(age < 18) {
+    alert("You are too young.")
+}else if(age > 50 && age <= 80){
+    alert("You are daninger")
+}else if(age > 80) {
+    alert("You are What ever")
 }
