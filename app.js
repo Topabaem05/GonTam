4ef052eb9a73b046092f5689ed33ef11920a214e
Topabaem05 <choijjs83@gmail.com>
2022-08-20 10:06:54 +0900

    Test

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const cal = {
     Plus: function (a,b){
         return a + b
-    },
-    Minos: function (a,b){
+    }, Minos: function (a,b){
         return a - b
-    },
-    Times: function (a,b){
+    },Times: function (a,b){
         return a * b
-    },
-    divide: function (a,b){
+    },divide: function (a,b){
         return a / b
-    },
-    Power: function (a,b){
+    },Power: function (a,b){
         return a ** b
     },
 };
-
 const result = cal.Plus(4,5)
 alert(result)
